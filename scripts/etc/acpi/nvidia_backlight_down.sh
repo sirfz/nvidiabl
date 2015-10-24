0be88a67ebfbfd0828c8b196baa54024459fad4e
FZ <iamfayez@gmail.com>
2015-10-24 19:32:12 +0300

    use nvidiablctl to control brightness

--- scripts/etc/acpi/nvidia_backlight_down.sh
+++ scripts/etc/acpi/nvidia_backlight_down.sh
@@ -2,14 +2,15 @@
 
 test -f /sys/class/backlight/nvidia_backlight/brightness || exit 0
 
-VAL=`cat /sys/class/backlight/nvidia_backlight/actual_brightness`
-MIN=0
-STEP=10
+nvidiablctl down
+# VAL=`cat /sys/class/backlight/nvidia_backlight/actual_brightness`
+# MIN=0
+# STEP=10
 
-VAL=`expr $VAL - $STEP`
+# VAL=`expr $VAL - $STEP`
 
-if [ $VAL -lt $MIN ]; then
-	VAL=$MIN
-fi
+# if [ $VAL -lt $MIN ]; then
+# 	VAL=$MIN
+# fi
 
-echo -n $VAL > /sys/class/backlight/nvidia_backlight/brightness
+# echo -n $VAL > /sys/class/backlight/nvidia_backlight/brightness
--- scripts/etc/acpi/nvidia_backlight_up.sh
+++ scripts/etc/acpi/nvidia_backlight_up.sh
@@ -2,14 +2,15 @@
 
 test -f /sys/class/backlight/nvidia_backlight/brightness || exit 0
 
-VAL=`cat /sys/class/backlight/nvidia_backlight/actual_brightness`
-MAX=`cat /sys/class/backlight/nvidia_backlight/max_brightness`
-STEP=10
+nvidiablctl up
+# VAL=`cat /sys/class/backlight/nvidia_backlight/actual_brightness`
+# MAX=`cat /sys/class/backlight/nvidia_backlight/max_brightness`
+# STEP=10
 
-VAL=`expr $VAL + $STEP`
+# VAL=`expr $VAL + $STEP`
 
-if [ $VAL -gt $MAX ]; then
-	VAL=$MAX
-fi
+# if [ $VAL -gt $MAX ]; then
+# 	VAL=$MAX
+# fi
 
-echo -n $VAL > /sys/class/backlight/nvidia_backlight/brightness
+# echo -n $VAL > /sys/class/backlight/nvidia_backlight/brightness
